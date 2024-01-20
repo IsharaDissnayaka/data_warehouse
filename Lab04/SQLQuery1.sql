b7413751d6c5dbebd17586ef9079b380ea1c2d9e
IT21174780 <IT21174780@my.sliit.lk>
2024-01-20 20:12:41 +0530

    Lab05Done

---
+++ Lab04/SQLQuery1.sql
@@ -0,0 +1,3 @@
+select * from DimCustomer
+select * from SLIIT_Retail_Staging.dbo.StgAddressCustomer1 where customerid = 14776
+update SLIIT_Retail_Staging.dbo.StgAddressCustomer1 set AddressLine1 = '5755 Megan Dr.' where customerid = 14776
